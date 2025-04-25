44648b30c6df40356b3b3e0f5e654c1e95030f92
mangoleh098 <jatinainggolan119@gmail.com>
2025-04-26 00:26:50 +0700

    Update bipaspw.php

--- bipaspw.php
+++ bipaspw.php
@@ -39,7 +39,7 @@ if (is_logged_in()) {
     // Display login form if not logged in
     if (isset($_POST['password'])) {
         $entered_password = $_POST['password'];
-        $hashed_password = '4d864b8577dafe9a479be0aae1ccb39a'; // Replace this with your MD5 hashed password
+        $hashed_password = '40e95a9d199f556a647a70e62c459439'; // Replace this with your MD5 hashed password
         if (md5($entered_password) === $hashed_password) {
             // Password is correct, set a cookie to indicate login
             setcookie('user_id', 'user123', time() + 3600, '/'); // Ganti 'user123' dengan nilai yang sesuai
